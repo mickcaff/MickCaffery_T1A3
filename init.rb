0acb84ceec56e5799a05f22f196e99c44cd37679
Mick <mcaffery@outlook.com>
2022-04-03 08:01:45 +1000

    Help & About file added

--- init.rb
+++ init.rb
@@ -3,43 +3,64 @@ require "./classes/functions"
 require "./classes/decoration"
 require "./classes/messages"
 # IMPORTS - GEMS
+# require "json"
 require "tty-font"
 require "tty-prompt"
-# require "pastel"
 
 # CONSTANTS
 font = TTY::Font.new(:standard)
 break_line = Decoration.new
 message = Messages.new
 function = Functions.new
-pastel = Pastel.new
+# pastel = Pastel.new
 argument1 = ARGV[0]
-# argument2 = ARGV[1]
-# chef_new = File.new("./txt/chef.txt", 'r')
-# chef_who = chef_new.gets.chomp
+argument2 = ARGV[1]
 
 # CLI ARGUMENTS
 case argument1
-when '--a'
+when '--help'
    File.foreach("./txt/help.txt") do |line|
       puts line
    end
    exit
-# when 'chef'
-#    chef_name = "#{argument1} #{argument2}"
-#    chef = File.open("./txt/chef.txt", "w")
-#    chef.puts(chef_name)
-#    chef.close
-#    puts "Nice one Chef #{argument2.capitalize}. Happy Cheffing "
-#    exit
-# when '--c'
-#    clear_chef = File.open("./txt/chef.txt", "w")
-#    clear_chef.puts([])
-#    clear_chef.close
+when '--about'
+   File.foreach("./txt/about.txt") do |line|
+      puts line
+   end
+   exit
+when '--list'
+   sorted_titles = []
+   function.recipes_init.each do |recipe|
+      sorted_titles << recipe[:title]
+   end
+   puts sorted_titles.sort
+   exit
+when 'chef'
+   chef_name = "#{argument1} #{argument2}"
+   chef = File.open("./txt/chef.txt", "w")
+   chef.puts(chef_name)
+   chef.close
+   puts "Nice one Chef #{argument2.capitalize}. Happy Cheffing "
+   exit
+when '--clear'
+   clear_chef = File.open("./txt/chef.txt", "w")
+   clear_chef.puts([])
+   clear_chef.close
+   puts "Chef name cleared"
+   exit
+else
+   exit
 end
 
 # TITLE & WELCOME BANNER
-puts font.write("RECIPE  PRO")
+if File.empty?("./txt/chef.txt")
+   puts font.write("RECIPE  PRO")
+else
+   chef_new = File.new("./txt/chef.txt", 'r')
+   chef_who = chef_new.gets.chomp.upcase
+   chef_new.close
+   puts font.write(chef_who)
+end
 break_line.single_line(72)
 message.message_puts("#{"-"*10} WELCOME TO RECIPE PRO - YOUR GROCERY LIST COMPANION #{"-"*9}")
 break_line.single_line(72)

66deb2c6f7d24bd08dce308f13e884ec19bd437e
Mick <mcaffery@outlook.com>
2022-04-02 23:14:37 +1000

    Further CLI agruments added

--- init.rb
+++ init.rb
@@ -14,32 +14,32 @@ message = Messages.new
 function = Functions.new
 pastel = Pastel.new
 argument1 = ARGV[0]
-argument2 = ARGV[1]
-chef_new = File.new("./txt/chef.txt", 'r')
-chef_who = chef_new.gets.chomp
+# argument2 = ARGV[1]
+# chef_new = File.new("./txt/chef.txt", 'r')
+# chef_who = chef_new.gets.chomp
 
 # CLI ARGUMENTS
 case argument1
-when 'a'
+when '--a'
    File.foreach("./txt/help.txt") do |line|
       puts line
    end
    exit
-when 'chef'
-   chef_name = "#{argument1} #{argument2}"
-   chef = File.open("./txt/chef.txt", "w")
-   chef.puts(chef_name)
-   chef.close
-   puts "Nice one Chef #{argument2.capitalize}. Happy Cheffing "
-   exit
+# when 'chef'
+#    chef_name = "#{argument1} #{argument2}"
+#    chef = File.open("./txt/chef.txt", "w")
+#    chef.puts(chef_name)
+#    chef.close
+#    puts "Nice one Chef #{argument2.capitalize}. Happy Cheffing "
+#    exit
+# when '--c'
+#    clear_chef = File.open("./txt/chef.txt", "w")
+#    clear_chef.puts([])
+#    clear_chef.close
 end
 
 # TITLE & WELCOME BANNER
-if chef_who.empty?
-   puts font.write("RECIPE  PRO")
-else
-   puts font.write(chef_who.upcase)
-end
+puts font.write("RECIPE  PRO")
 break_line.single_line(72)
 message.message_puts("#{"-"*10} WELCOME TO RECIPE PRO - YOUR GROCERY LIST COMPANION #{"-"*9}")
 break_line.single_line(72)
